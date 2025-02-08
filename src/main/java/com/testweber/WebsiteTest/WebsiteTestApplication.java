3a0bc432a82ca30bcd4b8aa28860c7cc7967ade3
Marcel Zama <C00260146@itcarlow.ie>
2025-02-08 16:57:32 +0000

    first commit

---
+++ src/main/java/com/testweber/WebsiteTest/WebsiteTestApplication.java
@@ -0,0 +1,13 @@
+package com.testweber.WebsiteTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsiteTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsiteTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testweber/WebsiteTest/WebsiteTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testweber.WebsiteTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
